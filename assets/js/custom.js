5f4fb601a122b132d393dbe437ff2cba8a744683
Shahriar Rhythm <rhythmshahriar0@gmail.com>
2022-05-01 09:49:57 +0600

    commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -28,13 +28,13 @@ jQuery(document).ready(function ($) {
     if ($('.owl-testimonials').length) {
         $('.owl-testimonials').owlCarousel({
             loop: true,
-            nav: false,
+            nav: true,
             dots: true,
             items: 1,
             margin: 30,
-            autoplay: false,
-            smartSpeed: 700,
-            autoplayTimeout: 6000,
+            autoplay: true,
+            smartSpeed: 150,
+            autoplayTimeout: 2000,
             responsive: {
                 0: {
                     items: 1,
@@ -50,37 +50,6 @@ jQuery(document).ready(function ($) {
                 },
                 992: {
                     items: 2,
-                    margin: 30
-                }
-            }
-        });
-    }
-
-    if ($('.owl-partners').length) {
-        $('.owl-partners').owlCarousel({
-            loop: true,
-            nav: false,
-            dots: true,
-            items: 1,
-            margin: 30,
-            autoplay: false,
-            smartSpeed: 700,
-            autoplayTimeout: 6000,
-            responsive: {
-                0: {
-                    items: 1,
-                    margin: 0
-                },
-                460: {
-                    items: 1,
-                    margin: 0
-                },
-                576: {
-                    items: 2,
-                    margin: 20
-                },
-                992: {
-                    items: 4,
                     margin: 30
                 }
             }
